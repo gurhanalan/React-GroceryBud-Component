f5898a5b7cb70faa3b3b7f18c9d00953b0bd7221
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-06 20:13:49 +0300

    Updates

---
+++ precache-manifest.beff934e7203bd4b583dffe2cb4e4fb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312f2c99a27aa959a0fcc20619cb9db5",
+    "url": "/React-GroceryBud-Component/index.html"
+  },
+  {
+    "revision": "51b01f5f9181ac94d703",
+    "url": "/React-GroceryBud-Component/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "1ee93fdcfb3c989c96d8",
+    "url": "/React-GroceryBud-Component/static/js/2.d4874bd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-GroceryBud-Component/static/js/2.d4874bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51b01f5f9181ac94d703",
+    "url": "/React-GroceryBud-Component/static/js/main.32092dd1.chunk.js"
+  },
+  {
+    "revision": "ea0bbf035880d0137078",
+    "url": "/React-GroceryBud-Component/static/js/runtime-main.f9431bf6.js"
+  }
+]);
